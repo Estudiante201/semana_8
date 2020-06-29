e551c0aadf5f1f49645341a89e903d8c6d88c7c2
Legion <Legion@192.168.1.61>
2020-06-29 17:44:58 -0500

    Crud alumno y concurso finalizado

---
+++ src/main/java/com/redsocial/repositorio/AlumnoRepositorio.java
@@ -0,0 +1,14 @@
+package com.redsocial.repositorio;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.redsocial.entidad.Alumno;
+
+public interface AlumnoRepositorio extends JpaRepository<Alumno, Integer>{
+	@Query("Select x from Alumno x where x.nombre like :var_nombre")
+	public abstract List<Alumno> buscaPorNombre(@Param("var_nombre") String filtro);
+}
---
+++ src/main/java/com/redsocial/servicio/AlumnoService.java
@@ -0,0 +1,12 @@
+package com.redsocial.servicio;
+
+import java.util.List;
+
+import com.redsocial.entidad.Alumno;
+
+public interface AlumnoService {
+	public abstract void eliminaAlumno(int id) ;
+	public abstract Alumno insertaActualizaAlumno(Alumno  obj) ;
+	public abstract List<Alumno> listaAlumno() ;
+	public abstract List<Alumno> listaAlumnoPorNombre(String filtro);
+}
